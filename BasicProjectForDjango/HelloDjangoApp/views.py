6af5445da344ab0d16fc1c7acaa24e70c177db97
Paul Sicking <sickingp@gmail.com>
2020-04-06 16:52:51 -0700

    Customize rendering of home page

--- BasicProjectForDjango/HelloDjangoApp/views.py
+++ BasicProjectForDjango/HelloDjangoApp/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render   # Added for this step
+from datetime import datetime
 
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+
+    return render(
+        request,
+        "HelloDjangoApp/index.html",  # Relative path from the 'templates' folder to the template file
+        # "index.html", # Use this code for VS 2017 15.7 and earlier
+        {
+            'title' : "Hello Django",
+            'message' : "Hello Django!!",
+            'content' : " on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
