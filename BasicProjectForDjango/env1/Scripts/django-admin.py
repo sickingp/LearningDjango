d0bb54ec365d30a0469614e4c5b261c2bc84a308
Paul Sicking <sickingp@gmail.com>
2020-04-06 20:58:27 -0700

    Create new Django project from Django Web Project template

---
+++ BasicProjectForDjango/env1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\sicki\Google Drive\VSStudio Projects\LearningDjango\BasicProjectForDjango\env1\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
